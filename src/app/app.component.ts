df6046533d566b675a231049a8b2ee641e6117b1
Dan Morris <dmorris@morrisdev.com>
2025-05-13 13:07:59 -0700

    Added home page and Auth0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component,   } from '@angular/core';
+import { Component, } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule, RouterModule ],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'GordonQA';
   isSidebarCollapsed = false;
+  idToken: any = null;
+
+
+  constructor(public auth: AuthService) { }
+
+  ngOnInit(): void {
+    this.auth.idTokenClaims$.subscribe(
+      {
+        next: (claims: any) => {
+          this.idToken = claims?.__raw || null;  // __raw contains the actual JWT
+          console.log('Raw ID Token:', this.idToken);
+        },
+        error: (err) => console.log('Error fetching ID Token:', err)
+      });
+
+   
+  }
+
+
+  logout() {
+
+    this.auth.logout({ logoutParams: { returnTo: document.location.origin } })
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,20 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import {  provideHttpClient , withInterceptors} from '@angular/common/http';
  
-  
+  import { provideAuth0 } from '@auth0/auth0-angular';
+
 import { authInterceptor } from './services/auth.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideAuth0({
+      domain: 'gordonqa.us.auth0.com',
+      clientId: '1PYWLeuSQGiqMP8ASBMe0zhfwqD5p43U',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
      provideRouter(routes, withComponentInputBinding()),
      provideHttpClient(withInterceptors([authInterceptor]))
     ]
