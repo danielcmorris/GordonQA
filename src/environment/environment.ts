90e4e9b65bf4f73de799d123e7c5b8a3b6b7cfa8
Dan Morris <dmorris@morrisdev.com>
2025-05-19 12:33:55 -0700

    updated ignore

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,8 @@
   
 export const environment = {
   production: false,
-  
+   serverURL: 'https://gordonapi2025'+'0512175224.azurewebs'+'ites.net',
+  commandServer:'https://gordonapi2025'+'0512175224.azurew'+'ebsites.net/api/cmd?co'+'de=4FDKxronsZTM0YTNqMaT0OORHp_HN'+'O9QPqioCtdXddP-AzFu68ZLVw==', 
+   
    
 }
